b4e714b182b7ed0acd3a587fb4e05069383c7de9
Aryan Yazdani <ayyazdaniaryan@gmail.com>
2017-05-04 16:44:26 +0200

    not much change here; just fix the indentation (from python to js)

--- assembl/static2/webpack.config.js
+++ assembl/static2/webpack.config.js
@@ -2,57 +2,57 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        bundle: ['babel-polyfill', './js/app/index'],
-        searchv1: ['babel-polyfill', './js/app/searchv1']
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: '[name].js',
-        publicPath: '/build/'
-    },
-    module: {
-        loaders: [
-        {
-            test: /\.jsx?(\?v=\d)?$/,
-            loaders: ['react-hot','babel'],
-            include: path.join(__dirname, 'js')
-        },
-        {
-            test: /\.scss$/,
-            loaders: ['style', 'css', 'sass']
-        },
-        { 
-            test: /\.css$/, 
-            loader: "style-loader!css-loader" 
-        },
-        { 
-            test: /\.png$/, 
-            loader: "url-loader?limit=100000" 
-        },
-        { 
-            test: /\.jpg$/, 
-            loader: "file-loader" 
-        },
-        {
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/, 
-            loader: 'url?limit=100000&name=[name].[ext]'
-        },
-        {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-            loader: 'file'
-        },
-        ]
-    },
-    resolve:{
-        extensions:['','.js','.jsx']
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify('production')
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin(),
+  entry: {
+    bundle: ['babel-polyfill', './js/app/index'],
+    searchv1: ['babel-polyfill', './js/app/searchv1']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?(\?v=\d)?$/,
+        loaders: ['react-hot','babel'],
+        include: path.join(__dirname, 'js')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/, 
+        loader: 'url?limit=100000&name=[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      }
     ]
+  },
+  resolve:{
+    extensions:['','.js','.jsx']
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
 };
--- assembl/static2/webpack.dev.config.js
+++ assembl/static2/webpack.dev.config.js
@@ -8,28 +8,28 @@ var webpackHost = process.env.WEBPACK_URL.split('://')[1].split(':')[0];
 var config = Object.create(baseConfig);
 
 config.devServer = {
-    inline: true,
-    headers: {
-        "Access-Control-Allow-Origin": process.env.ASSEMBL_URL,
-        "Access-Control-Allow-Credentials":true
-    },
-    port: webpackPort,
-    host: webpackHost
+  inline: true,
+  headers: {
+    "Access-Control-Allow-Origin": process.env.ASSEMBL_URL,
+    "Access-Control-Allow-Credentials":true
+  },
+  port: webpackPort,
+  host: webpackHost
 };
 config.entry = {
-    bundle: [
-        'webpack-dev-server/client?' + process.env.WEBPACK_URL,
-        'webpack/hot/only-dev-server',
-        './js/app/index'
-    ],
-    searchv1: [
-        'webpack-dev-server/client?' + process.env.WEBPACK_URL,
-        'webpack/hot/only-dev-server',
-        './js/app/searchv1'
-    ]
+  bundle: [
+    'webpack-dev-server/client?' + process.env.WEBPACK_URL,
+    'webpack/hot/only-dev-server',
+    './js/app/index'
+  ],
+  searchv1: [
+    'webpack-dev-server/client?' + process.env.WEBPACK_URL,
+    'webpack/hot/only-dev-server',
+    './js/app/searchv1'
+  ]
 };
 config.plugins = [
-    new webpack.HotModuleReplacementPlugin()
+  new webpack.HotModuleReplacementPlugin()
 ];
 
 module.exports = config;
