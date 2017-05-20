2e9e7b4a9f0395a8d21470cae26e4a08e12e92f0
paolina92 <paolinardc@gmail.com>
2017-05-20 14:30:41 +0200

    Create tabs in section 2 + create video component

--- assembl/static2/js/app/utils/translations.js
+++ assembl/static2/js/app/utils/translations.js
@@ -187,6 +187,8 @@ const Translations = {
       panelTitle: 'Conditions Générales d\'Utilisations'
     },
     administration: {
+      videoModule: 'Option module vidéo',
+      thematic: 'Thématique',
       edition: 'Discussion',
       landingpage: 'Landing page',
       survey: ['Créer les thématiques', 'Renseigner les thématiques', 'Exporter les données'],
@@ -395,6 +397,8 @@ const Translations = {
       panelTitle: 'Terms and Conditions'
     },
     administration: {
+      videoModule: 'Video module',
+      thematic: 'Theme',
       edition: 'Discussion',
       landingpage: 'Landing page',
       survey: ['Create themes', 'Edit themes', 'Export data'],
